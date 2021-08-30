ad44dacd3c2dfcb08cd557b2276d4da691f4d5fb
Adi19471 <akumatha@gmail.com>
2021-08-30 18:30:37 +0530

    gallery page saved

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import *
 from django.contrib.auth import authenticate, login, logout
+
+
+
 # Create your views here.
 # registration
 def register(request):
